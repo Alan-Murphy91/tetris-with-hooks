3caca8da416d110fb7c5eb9dc0aa5db795abc77c
Alan <alanjmurphy91@gmail.com>
2019-09-26 11:07:56 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from './redux/store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
-// const store = configureStore();
-
-// const App = (
-// 	<Provider store={store}>
-// 		<AppRouter />
-// 	</Provider>
-// );
 
 const App = <AppRouter />
 
